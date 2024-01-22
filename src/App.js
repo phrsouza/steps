a0579db2f75502070048c4f1e471bafa8c839a2e
Fauno <pedrohenriqueramossouza@gmail.com>
2024-01-22 13:22:24 -0300

    dynamically build steps based on messages

--- src/App.js
+++ src/App.js
@@ -25,9 +25,11 @@ export default function App() {
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
+        {messages.map((message, index) => {
+          return (
+            <div className={step >= index + 1 ? "active" : ""}>{index + 1}</div>
+          );
+        })}
       </div>
 
       <p className="message">
