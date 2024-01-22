94ea8989dd9d324236794ac2a04d1e920fe3c06d
Fauno <pedrohenriqueramossouza@gmail.com>
2024-01-22 13:04:42 -0300

    add static structure

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
-  return <div>Hello React</div>;
+  const step = 1;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7850f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7850f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
